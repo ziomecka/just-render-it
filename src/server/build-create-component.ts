47a38b10fd79e0969ad1fc600578f4797578dca5
Katarzyna Ziomek-Zdanowicz <ziomecka78@gmail.com>
2019-10-29 10:38:19 +0100

    Uses BuildCreateComponent in server's builder, passes globals to common builder

--- src/server/build-create-component.ts
+++ src/server/build-create-component.ts
@@ -1,18 +1,25 @@
 import {
-  CreateComponent,
-  Helpers,
+  BuildCreateComponent,
   buildCreateComponent as commonBuilder,
 } from '../common/';
 
 const render = require('rendering-recursive-server-side').default;
 
-const buildCreateComponent = async (
-  { classNames = {}, options = [] } = {}
-): Promise<{createComponent: CreateComponent; helpers: Helpers}> => {
-  return commonBuilder({
+const buildCreateComponent: BuildCreateComponent = async (
+  {
+    inject: { classNames = {} } = {},
+    options = [],
+    globals = {
+      createComponent: true,
+      helpers: false,
+    },
+  } = {}
+) => {
+  return await commonBuilder({
     render,
     inject: { classNames },
     options,
+    globals,
   });
 };
 
