76a10955c4d4b16bdf8b0b38fa45d487af13b697
Katarzyna Ziomek-Zdanowicz <ziomecka78@gmail.com>
2019-10-29 10:41:18 +0100

    Creates global window and document in sever's buildCreateComponent

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,28 @@
-export {
-  buildCreateComponent,
-} from './server/';
+import { Helpers, CreateComponent } from './common/'
+import { buildCreateComponent as builder } from './server/';
+
+export const buildCreateComponent = (
+  async ({ classNames = {}, options = [] } = {}): Promise<{
+    createComponent: CreateComponent,
+    helpers: Helpers,
+  }> => {
+    const fakeFunction = (): void => {};
+
+    global.window = {
+      addEventListener: fakeFunction,
+      removeEventListener: fakeFunction,
+      document: {
+        addEventListener: fakeFunction,
+        removeEventListener: fakeFunction,
+      },
+    };
+
+    global.document = {
+      getElementById: fakeFunction,
+      getElementsByClassName: fakeFunction,
+      getElementsByTagName: fakeFunction,
+    };
+
+    return await builder({ inject: { classNames }, options });
+  }
+);
